be62c88d41912ebb2ef801e5a97c89cbe789cf0f
AllanD1 <allanweb019@gmail.com>
2017-07-22 15:23:36 -0300

    aula pos

---
+++ AulaPos/Adm/Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Adm_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label Text="Login:" runat="server" /> <br />
+        <asp:TextBox runat="server" ID="txtLogin"  /> <br />
+        <asp:Label Text="Senha:" runat="server" /> <br />
+        <asp:TextBox runat="server" ID="txtSenha" TextMode="Password" /> <br />
+
+        <asp:Button id="btnLogin" Text="Entrar" runat="server" OnClick="btnLogin_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ AulaPos/App_Code/Usuario.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Usuario
+/// </summary>
+public class Usuario
+{
+    #region
+    public int usu_id { get; set; }
+    public string usu_nome { get; set; }
+    public string usu_login { get; set; }
+    public string usu_senha{ get; set; }
+    public bool usu_ativo { get; set; }
+   
+   
+    #endregion
+
+
+    public Usuario()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+
+    public Usuario(int pUsuId) 
+    {
+        DataSet.TbUsuarioDataTable tbUsuario = new DataSet.TbUsuarioDataTable();
+        DataSetTableAdapters.TbUsuarioTableAdapter dsUsuario = new DataSetTableAdapters.TbUsuarioTableAdapter();
+        dsUsuario.FillByUsuId(tbUsuario, pUsuId);
+
+        if (tbUsuario.Rows.Count > 0)
+        {
+            DataSet.TbUsuarioRow regUsuario = (DataSet.TbUsuarioRow)tbUsuario.Rows[0];
+
+            this.usu_id = regUsuario.usu_id;
+            this.usu_nome = regUsuario.usu_nome;
+            this.usu_login = regUsuario.usu_login;
+            this.usu_senha = regUsuario.usu_senha;
+            this.usu_ativo = regUsuario.usu_ativo;
+        }
+    }
+}
